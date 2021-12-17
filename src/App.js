d23f5bd34b39e78c060f17d022bd674a5ada5e82
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2021-12-17 12:02:09 -0700

    finished creating form, created initial form values

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Link, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import OrderPizza from "./components/OrderPizza";
+import axios from "axios";
+
+const initialFormValues = {
+  name: "",
+  size: "",
+  pepperoni: false,
+  sausage: false,
+  pineapple: false,
+  tomatoes: false,
+  olives: false,
+};
+const initialFormErrors = {
+  name: "",
+};
 
 const App = () => {
+  const [orders, setOrders] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+
+  const getOrders = () => {
+    axios
+      .get("http://fakeapi.com")
+      .then((resp) => {
+        setOrders(resp.data);
+      })
+      .catch((err) => console.error(err)) }, []);
+
+  // const createNewOrder = (newOrder) => {
+  //   axios
+  //     .post("http://fakeapi.com/api/orders", newOrder)
+  //     .then((resp) => {
+  //       setOrders([resp.data, ...orders]);
+  //     })
+  //     .catch((err) => console.error(err))
+  //     .finally(() => setFormValues(initialFormValues));
+  // };
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
   return (
     <div className="page-container">
       <header>
--- src/components/OrderPizza.js
+++ src/components/OrderPizza.js
@@ -1,6 +1,19 @@
 import React from "react";
 
 export default function OrderPizza(props) {
+  const { values, submit, change, disabled, errors } = props;
+
+  const onChange = (evt) => {
+    const { name, value, checked, type } = evt.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    change(name, valueToUse);
+  };
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
+
   return (
     <div className="order-pizza-form">
       <h1>Build Your Pizza</h1>
@@ -10,25 +23,91 @@ export default function OrderPizza(props) {
         className="pizza-order-img"
       ></img>
       <h2>Build Your Own Pizza</h2>
-      <form className="form-container">
+      <form className="form-container" id="pizza-form" onSubmit={onSubmit}>
         <div className="form-inputs">
-          <label></label>
-          <label htmlFor="size">
+          <label>
+            Name
+            <input
+              id="name-input"
+              name="name-input"
+              type="text"
+              placeholder="type your name"
+            />
+          </label>
+          <label
+            onChange={onChange}
+            values={values.size}
+            id="size-dropdown"
+            name="size-dropdown"
+          >
             <p className="regular-title">Choice of Size</p>
             <p className="small-title">Required</p>
+            <select name="size">
+              <option value="">-- Select --</option>
+              <option value="Small">Small</option>
+              <option value="Medium">Medium</option>
+              <option value="Large">Large</option>
+              <option value="Extra Large">Extra Large</option>
+            </select>
           </label>
-          <select name="size" id="size">
-            <option value="">-- Select --</option>
-            <option value="Small">Small</option>
-            <option value="Medium">Medium</option>
-            <option value="Large">Large</option>
-            <option value="Extra Large">Extra Large</option>
-          </select>
-          <label htmlFor="sauce">
-            <p className="regular-title">Choice of Sauce</p>
-            <p className="small-title">Required</p>
+          <label>
+            Pepperoni
+            <input
+              type="checkbox"
+              name="pepperoni"
+              checked={values.pepperoni}
+              onChange={onChange}
+            />
           </label>
-          <
+
+          <label>
+            Sausage
+            <input
+              type="checkbox"
+              name="sausage"
+              checked={values.sausage}
+              onChange={onChange}
+            />
+          </label>
+
+          <label>
+            Pineapple
+            <input
+              type="checkbox"
+              name="pineapple"
+              checked={values.pineapple}
+              onChange={onChange}
+            />
+          </label>
+          <label>
+            Tomatoes
+            <input
+              type="checkbox"
+              name="tomatoes"
+              checked={values.tomatoes}
+              onChange={onChange}
+            />
+          </label>
+          <label>
+            Olives
+            <input
+              type="checkbox"
+              name="olives"
+              checked={values.olives}
+              onChange={onChange}
+            />
+          </label>
+          <label>
+            Special Instructions
+            <input
+              value={values.special}
+              onChange={onChange}
+              name="special"
+              id="special-text"
+              type="text"
+            />
+          </label>
+          <button disabled={disabled}>Add to Order</button>
         </div>
       </form>
     </div>
